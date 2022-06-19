a9dc76d974c63ed0a5e7ad1040e262d6887963b3
yash140920 <88269072+yash140920@users.noreply.github.com>
2022-06-19 09:39:44 +0530

    Add files via upload

---
+++ question2.js
@@ -0,0 +1,34 @@
+const quizData = [
+    {
+        question: "In which year javascript was launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "none of the above",
+        correct: "b",
+    },
+    {
+        question: "What does CSS stand for?",
+        a: "Central Style Sheets",
+        b: "Cascading Style Sheets",
+        c: "Cascading Simple Sheets",
+        d: "Cars SUVs Sailboats",
+        correct: "b",
+    },
+    {
+        question: "Who inventd CSS?",
+        a: "Hakon Wium Lie",
+        b: "Sergey Brin",
+        c: "Rasmus Lerdorf",
+        d: "Brendan Eich",
+        correct: "a",
+    },
+    {
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "none of the above",
+        correct: "b",
+    },
+];
